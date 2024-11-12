3d2437e9fdcabfe629466205153710ee5dde2c60
titis69 <117496052+titis69@users.noreply.github.com>
2024-11-12 12:58:29 +0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -55,6 +55,7 @@ res1() {
     wget https://raw.githubusercontent.com/titis69/scalfa//main/menu/backup
     wget https://raw.githubusercontent.com/titis69/scalfa//main/menu/regis
     wget https://raw.githubusercontent.com/titis69/scalfa//main/menu/addhost
+    wget https://raw.githubusercontent.com/titis69/scalfa//main/menu/menu-x
     chmod +x menu
     chmod +x m-sshws
     chmod +x addssh
@@ -64,7 +65,7 @@ res1() {
     chmod +x backup
     chmod +x regis
     chmod +x addhost
-    
+    chmod +x menu-x
    
 }
 function noobzvpn() {
