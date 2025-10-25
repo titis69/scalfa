9f87e6b77c2735b13843e655e24b52de951efa47
titis69 <117496052+titis69@users.noreply.github.com>
2025-10-26 02:50:56 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -70,12 +70,12 @@ read -p "$( echo -e "Press ${GRAY}[ ${NC}${green}Enter${NC} ${GRAY}]${NC} For St
 echo ""
 clear
 if [ "${EUID}" -ne 0 ]; then
-        echo "You need to run this script as root"
-        exit 1
+		echo "You need to run this script as root"
+		exit 1
 fi
 if [ "$(systemd-detect-virt)" == "openvz" ]; then
-        echo "OpenVZ is not supported"
-        exit 1
+		echo "OpenVZ is not supported"
+		exit 1
 fi
 red='\e[1;31m'
 green='\e[0;32m'
@@ -100,9 +100,9 @@ function print_ok() {
     echo -e "${OK} ${BLUE} $1 ${FONT}"
 }
 function print_install() {
-    echo -e "${green} =============================== ${FONT}"
+	echo -e "${green} =============================== ${FONT}"
     echo -e "${YELLOW} # $1 ${FONT}"
-    echo -e "${green} =============================== ${FONT}"
+	echo -e "${green} =============================== ${FONT}"
     sleep 1
 }
 
@@ -112,9 +112,9 @@ function print_error() {
 
 function print_success() {
     if [[ 0 -eq $? ]]; then
-        echo -e "${green} =============================== ${FONT}"
+		echo -e "${green} =============================== ${FONT}"
         echo -e "${Green} # $1 berhasil dipasang"
-        echo -e "${green} =============================== ${FONT}"
+		echo -e "${green} =============================== ${FONT}"
         sleep 2
     fi
 }
@@ -254,13 +254,11 @@ read -p "   Subdomain: " host1
 echo "IP=" >> /var/lib/kyt/ipvps.conf
 echo $host1 > /etc/xray/domain
 echo $host1 > /root/domain
-echo "BERLIAN" > /etc/xray/username
 echo ""
 elif [[ $host == "2" ]]; then
 #install cf
 wget ${REPO}files/cf.sh && chmod +x cf.sh && ./cf.sh
 rm -f /root/cf.sh
-echo "BERLIAN" > /etc/xray/username
 clear
 else
 print_install "Random Subdomain/Domain is Used"
@@ -313,9 +311,9 @@ else
 sts="${Error}"
 fi
 TIMES="10"
-CHATID="1507094635"
-KEY="7058358156:AAHL7dXE47BwT9L80yjXm2NfcOtuirVRITs"
-URL="https://api.telegram.org/bot$KEY/sendMessage"
+#CHATID="1469244768"
+#KEY="7035119506:AAEatu58omJunqReXanKi1mRZZgAcFkE7XQ"
+#URL="https://api.telegram.org/bot$KEY/sendMessage"
 ISP=$(cat /root/.isp)
 CITY=$(cat /root/.city)
 TIMEZONE=$(printf '%(%H:%M:%S)T')
@@ -333,7 +331,7 @@ TIMEZONE=$(printf '%(%H:%M:%S)T')
 <b>   BERLIAN VPN STORE SCRIPT     </b>
 <code>─────────────────────────────</code>
 <i>Automatic Notifications From Github</i>
-#"'&reply_markup={"inline_keyboard":[[{"text":"ᴏʀᴅᴇʀ","url":"t.me/ohmyvillain"}]]}' 
+"'&reply_markup={"inline_keyboard":[[{"text":"ᴏʀᴅᴇʀ","url":"t.me/ohmyvillain"}]]}' 
 
     curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
 }
@@ -590,8 +588,10 @@ clear
 function ins_dropbear(){
 clear
 print_install "Menginstall Dropbear"
+# // Installing Dropbear
 apt-get install dropbear -y > /dev/null 2>&1
 wget -q -O /etc/default/dropbear "${REPO}config/dropbear.conf"
+# // wget -q -O /etc/default/dropbear https://raw.githubusercontent.com/titis69/scalfa/main/config/dropbear.conf
 chmod +x /etc/default/dropbear
 /etc/init.d/dropbear restart
 /etc/init.d/dropbear status
@@ -712,10 +712,10 @@ print_install "Menginstall Fail2ban"
 
 # Instal DDOS Flate
 if [ -d '/usr/local/ddos' ]; then
-    echo; echo; echo "Please un-install the previous version first"
-    exit 0
+	echo; echo; echo "Please un-install the previous version first"
+	exit 0
 else
-    mkdir /usr/local/ddos
+	mkdir /usr/local/ddos
 fi
 
 clear
@@ -840,43 +840,43 @@ EOF
 mkdir -p /root/.info
 curl -sS "ipinfo.io/org?token=7a814b6263b02c" > /root/.info/.isp
 cat >/etc/cron.d/xp_all <<-END
-        SHELL=/bin/sh
-        PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-        2 0 * * * root /usr/local/sbin/xp
-    END
-    cat >/etc/cron.d/logclean <<-END
-        SHELL=/bin/sh
-        PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-        */20 * * * * root /usr/local/sbin/clearlog
-        END
+		SHELL=/bin/sh
+		PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+		2 0 * * * root /usr/local/sbin/xp
+	END
+	cat >/etc/cron.d/logclean <<-END
+		SHELL=/bin/sh
+		PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+		*/20 * * * * root /usr/local/sbin/clearlog
+		END
     chmod 644 /root/.profile
-    
+	
     cat >/etc/cron.d/daily_reboot <<-END
-        SHELL=/bin/sh
-        PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-        0 5 * * * root /sbin/reboot
-    END
+		SHELL=/bin/sh
+		PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+		0 5 * * * root /sbin/reboot
+	END
     cat >/etc/cron.d/limit_ip <<-END
-        SHELL=/bin/sh
-        PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-        */2 * * * * root /usr/local/sbin/limit-ip
+		SHELL=/bin/sh
+		PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+		*/2 * * * * root /usr/local/sbin/limit-ip
         END
     cat >/etc/cron.d/lim-ip-ssh <<-END
-        SHELL=/bin/sh
-        PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-        */1 * * * * root /usr/local/sbin/limit-ip-ssh
-    END
+		SHELL=/bin/sh
+		PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+		*/1 * * * * root /usr/local/sbin/limit-ip-ssh
+	END
     cat >/etc/cron.d/limit_ip2 <<-END
-        SHELL=/bin/sh
-        PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-        */2 * * * * root /usr/bin/limit-ip
-    END
+		SHELL=/bin/sh
+		PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+		*/2 * * * * root /usr/bin/limit-ip
+	END
     echo "*/1 * * * * root echo -n > /var/log/nginx/access.log" >/etc/cron.d/log.nginx
     echo "*/1 * * * * root echo -n > /var/log/xray/access.log" >>/etc/cron.d/log.xray
     service cron restart
     cat >/home/daily_reboot <<-END
-        5
-    END
+		5
+	END
 curl -sS "ipinfo.io/city?token=7a814b6263b02c" > /root/.info/.city
 cat >/etc/systemd/system/rc-local.service <<EOF
 [Unit]
@@ -1025,4 +1025,3 @@ echo "Semuanya Berjalan Ok...!!"
 sleep 1
 echo -ne "[ ${yell}COMPLETED${NC} ] PENGINSTALAN SCRIPT SELESAI? "
 reboot
-
